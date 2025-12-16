cc1892c5d01eb0caf4995b22f24abfaa6eef35d1
indunadella <indu.nadella1122@gmail.com>
2025-12-16 09:31:29 +0530

    reset voter migrations

--- VoterCard/settings.py
+++ VoterCard/settings.py
@@ -147,3 +147,7 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_URL='/media/'
 MEDIA_ROOT=BASE_DIR/'media'
+
+MIGRATION_MODULES = {
+    'voter': None,
+}
