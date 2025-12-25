ae9339ec9b86f5720aa8e96ee772fd19e8064514
indunadella <indu.nadella1122@gmail.com>
2025-12-25 21:32:11 +0530

    final

--- VoterCard/settings.py
+++ VoterCard/settings.py
@@ -148,8 +148,8 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-# MEDIA_URL='/media/'
-# MEDIA_ROOT=BASE_DIR/'media'
+MEDIA_URL='/media/'
+MEDIA_ROOT=BASE_DIR/'media'
 
 CLOUDINARY_STORAGE={
     'CLOUD_NAME':os.environ.get('CLOUDINARY_CLOUD_NAME'),
